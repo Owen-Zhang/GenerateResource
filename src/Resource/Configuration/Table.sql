5f04d3ede3342868d5723d0162b349e1c68d965f
Owen-Zhang <1034992601@qq.com>
2017-03-26 21:28:05 +0800

    chang sql

--- src/Resource/Configuration/Table.sql
+++ src/Resource/Configuration/Table.sql
@@ -2,6 +2,6 @@
    transactionNumber INT NOT NULL AUTO_INCREMENT,
    KeyWord VARCHAR(50) NOT NULL,
    Chinese VARCHAR(1000) NOT NULL,
-   English VARCHAR(1000) NOT NULL,
+   English VARCHAR(1000),
    PRIMARY KEY (transactionNumber)
    )
